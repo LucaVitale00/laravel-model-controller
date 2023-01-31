3f2d86ddb20c033c25af5ae6a6076b4f2a3d7019
LucaVitale00 <luca16vitale2000@gmail.com>
2023-01-31 18:25:27 +0100

    add controller, model and films list

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel movies db</title>
+</head>
+<body>
+    <h1>List Movies</h1>
+
+    <ul>
+        @foreach ($movies as $movie)
+        <li>{{ $movie['title']}} - {{ $movie['original_title']}} - {{ $movie['nationality']}} - {{ $movie['date']}} - {{ $movie['vote']}}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
